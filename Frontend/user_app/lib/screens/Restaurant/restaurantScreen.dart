da0c53dab19563496a22239b0407ba1450aaa1bf
NikhilD1998 <nikhild.dev.1998@gmail.com>
2024-05-26 13:31:38 +0530

    Added Slider Menu

--- Frontend/user_app/lib/screens/Restaurant/restaurantScreen.dart
+++ Frontend/user_app/lib/screens/Restaurant/restaurantScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 import 'package:user_app/Screens/Restaurant/cartScreen.dart';
 import 'package:user_app/Widgets/buttons/viewCartButton.dart';
@@ -8,9 +9,10 @@ import 'package:user_app/Widgets/customs/Food/searchBar.dart';
 import 'package:user_app/Widgets/customs/Restaurants/foodItemWidget.dart';
 import 'package:user_app/Widgets/customs/Restaurants/foodTypeToggle.dart';
 import 'package:user_app/Widgets/customs/Restaurants/restaurantMenuScroll.dart';
+import 'package:user_app/utils/dummy_data.dart';
 
 class RestaurantScreen extends StatefulWidget {
-  const RestaurantScreen({super.key});
+  const RestaurantScreen({Key? key}) : super(key: key);
 
   @override
   State<RestaurantScreen> createState() => _RestaurantScreenState();
@@ -19,9 +21,12 @@ class RestaurantScreen extends StatefulWidget {
 class _RestaurantScreenState extends State<RestaurantScreen> {
   List<bool> isFilterSelected = [false, false, false, false, false];
   int _cartCounter = 0;
+  bool _isFloatingContainerVisible = false;
 
   List<bool> isMenuSelected = [false, false, false, false, false, false, false];
   int selectedMenuIndex = -1;
+
+  int selectedIndex = 0;
 
   void toggleSelection(int index) {
     setState(() {
@@ -48,34 +53,6 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
         selectedMenuIndex = index;
       }
     });
-  }
-
-  // Function for unselected text
-  Widget unSelectedText(String data) {
-    return Text(
-      data,
-      style: TextStyle(
-        fontFamily: 'Poppins',
-        fontWeight: FontWeight.normal,
-        fontSize: 10.sp,
-        color: textGrey2,
-      ),
-    );
-  }
-
-  // Function for selected text
-  Widget selectedText(String data) {
-    return Text(
-      data,
-      style: TextStyle(
-        fontFamily: 'Poppins',
-        fontWeight: FontWeight.bold,
-        fontSize: 12.sp,
-        color: primaryColor,
-      ),
-      maxLines: 2,
-      textAlign: TextAlign.center,
-    );
   }
 
   routeCart() {
@@ -110,49 +87,126 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
         ],
         centerTitle: true,
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 2.w),
-            child: searchBar('Search in Shiva Chinese Wok'),
-          ),
-          foodTypeToggle(toggleSelection, isFilterSelected),
-          Stack(
-            children: [
-              Container(
-                height: 73.h,
-                margin: EdgeInsets.only(right: 1.w),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    restaurantMenuScroll(isMenuSelected, handleMenuSelection),
-                    Container(
-                      width: 76.w,
-                      color: bgColor,
-                      child: ListView.builder(
-                        itemCount: 10,
-                        itemBuilder: (context, index) {
-                          return FoodItemWidget(
-                            onCounterChanged: updateCartCounter,
-                          );
-                        },
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 2.w),
+                  child: searchBar('Search in Shiva Chinese Wok'),
+                ),
+                foodTypeToggle(toggleSelection, isFilterSelected),
+                Container(
+                  height: 73.h,
+                  margin: EdgeInsets.only(right: 1.w),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      restaurantMenuScroll(isMenuSelected, handleMenuSelection),
+                      Container(
+                        width: 76.w,
+                        color: bgColor,
+                        child: ListView.builder(
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            return FoodItemWidget(
+                              onCounterChanged: updateCartCounter,
+                            );
+                          },
+                        ),
                       ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 1.h,
+                  left: 1.w,
+                  right: 1.w,
+                  child: Center(
+                    child: _cartCounter > 0
+                        ? viewCartButton(_cartCounter, routeCart)
+                        : const SizedBox.shrink(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (_isFloatingContainerVisible) // Conditionally show the container
+            Positioned(
+              bottom: 60,
+              right: 25.w,
+              child: Container(
+                width: 250,
+                height: 400,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(7),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        for (int index = 0;
+                            index < menuCategories.length;
+                            index++)
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.all(5),
+                                child: InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedIndex =
+                                          index; // Update selectedIndex when a menu category is tapped
+                                    });
+                                  },
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '${menuCategories[index].name}',
+                                        style: TextStyle(
+                                          color: index == selectedIndex
+                                              ? Colors.red
+                                              : Colors.black,
+                                          fontWeight: index == selectedIndex
+                                              ? FontWeight.bold
+                                              : FontWeight.normal,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      Text(
+                                        '${menuCategories[index].itemCount}',
+                                        style: TextStyle(
+                                          color: index == selectedIndex
+                                              ? Colors.red
+                                              : Colors.black,
+                                          fontWeight: index == selectedIndex
+                                              ? FontWeight.bold
+                                              : FontWeight.normal,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Divider(
+                                color: Colors.black,
+                              ),
+                            ],
+                          ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-              Positioned(
-                bottom: 1.h,
-                left: 1.w,
-                right: 1.w,
-                child: Center(
-                  child: _cartCounter > 0
-                      ? viewCartButton(_cartCounter, routeCart)
-                      : const SizedBox.shrink(),
-                ),
-              ),
-            ],
-          )
+            ),
         ],
       ),
       floatingActionButton: Padding(
@@ -162,18 +216,29 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
           height: 5.h,
           width: 20.w,
           child: FloatingActionButton.extended(
-            backgroundColor: primaryColor,
+            backgroundColor:
+                _isFloatingContainerVisible ? Colors.black : primaryColor,
             foregroundColor: primaryColor,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                _isFloatingContainerVisible = !_isFloatingContainerVisible;
+              });
+            },
             label: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset('assets/icons/menu_icon.png'),
+                _isFloatingContainerVisible
+                    ? Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      )
+                    : Image.asset('assets/icons/menu_icon.png'),
                 SizedBox(width: 2.w),
                 Text(
-                  'MENU',
+                  _isFloatingContainerVisible ? 'CLOSE' : "MENU",
                   style: h6TextStyle.copyWith(color: Colors.white),
                 ),
               ],
